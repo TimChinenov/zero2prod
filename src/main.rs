bc4b50b77cf051da041237525449e6260a4125cc
TimChinenov <tim.chinenov@gmail.com>
2022-09-19 22:05:43 -0700

    added integrations tests for subscription api

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use std::net::TcpListener;
+use zero2prod::run;
 
 // we need main to be async, but by default main cannot be async. 
 // async in rust uses futures - (a value that may not be there yet)
@@ -16,22 +8,6 @@ async fn health_check() -> impl Responder {
 // Macros are responsible for code generation.
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // controls where the application should be listening for requests
-    // What is the maximum number of concurrent connections that we should allow
-    // How many new connections per unit of time?
-    // Do we enable Transport Layer Security (TLS)
-    // transport level concerns
-    HttpServer::new( || {
-        // application level logic lives here
-        // routing, middleware, request handlers, 
-        App::new()
-            // routes take a path, and a route instances object. A Route is a Handler with a set of gaurds
-            // web::get() is a short cut for Router::new().gaurd(gaurd::Get())
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health-check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
